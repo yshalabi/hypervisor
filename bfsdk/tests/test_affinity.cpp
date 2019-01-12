c587c69f40c7fd79070aec07ee2627a2f260c40d
Yasser Shalabi <yassershalabi@gmail.com>
2019-01-12 14:15:52 -0600

    Update test_affinity.cpp
    Potentially incorrect assumption regarding number of threads available will cause this unit-test to incorrectly fail. Instead, use platform independent std::thread::hardware_concurrency to discover number of threads available.

--- bfsdk/tests/test_affinity.cpp
+++ bfsdk/tests/test_affinity.cpp
@@ -18,9 +18,10 @@
 
 #include <catch/catch.hpp>
 #include <bfaffinity.h>
+#include <thread>
 
 TEST_CASE("set_affinity")
 {
     CHECK(set_affinity(0) == 0);
-    CHECK(set_affinity(31) == -1);
+    CHECK(set_affinity(std::thread::hardware_concurrency()) == -1);
 }
